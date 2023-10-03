41f9cb37c427ec8fc5f3ec625a778148234cc5d2
yohoMobile <mobile@yohopartner.com>
2023-10-03 21:41:12 +0545

    create folder structure

---
+++ src/const/Colors.tsx
@@ -0,0 +1,19 @@
+const Colors = {
+    bgColor: '#28B4EB',
+    mainColor: '#CD0100',
+    whiteColor: '#ffffff',
+    yellowColor: '#E8D001',
+    fbColor: '#374195',
+    introColor: '#59b2ab',
+    tomatoColor: '#FF644C',
+    blumine: '#1B597E',
+    blackColor: '#000000',
+    greenColor: '#00BC22',
+    redColor: '#ff0000',
+    lightGrayColor: '#D3D3D3',
+    veryLightGrayColor: '#F0F3F4',
+    dimGrayColor: "#909090",
+    lightDimWhiteColor: '#F1F0F0'
+};
+
+export { Colors };
